7c74cb99dd3cae7a985c97819b92e9d3a14bbed8
Julia Leonoff <julia.leonoff@vanderbilt.edu>
2020-06-15 20:21:41 -0400

    table!

--- static/table.js
+++ static/table.js
@@ -1,32 +1,38 @@
-d3.json("http://127.0.0.1:5000/movie_data").then((data) => {
-    console.log(data);
+function makeTable(data) {
+   
     var tbody = d3.select("tbody");
-
+   
     data.forEach(function(movies) {
+        
         var row = tbody.append("tr");
-        Object.entries(movies).forEach(function([key, value]) 
-        {
+        Object.entries(movies).forEach(function([key, value]) {
             var cell = row.append("td");
             cell.text(value);
-            })
         });
     });
+};
 
+d3.json("http://127.0.0.1:5000/movie_data").then((data) => {
+    
+    console.log(data);
+    makeTable(data);
 
+});
 
 var button = d3.select("#filter-btn");
 
 function PressButton()
     {
     d3.event.preventDefault();
+
     tableData = d3.json("http://127.0.0.1:5000/movie_data").then((data) => {
         console.log(`promiseResult ${data}`);
     
         var inputTitle = d3.select("#title").property("value");
-        var inputGenre = d3.select("#genre").property("value").charAt(0).toUpperCase();
-        var inputYear = d3.select("#year").property("value").charAt(0).toUpperCase();
+        var inputGenre = d3.select("#genre").property("value");
+        var inputYear = d3.select("#year").property("value");
         var inputRating = d3.select("#rating").property("value");
-        var inputDirector = d3.select("#director").property("value").charAt(0).toUpperCase();
+        var inputDirector = d3.select("#director").property("value");
 
         let inputValues = [
             [inputTitle, 'movie'],
@@ -51,29 +57,21 @@ function PressButton()
 
         Object.entries(inputValues).forEach((k)=>{
             console.log(k[1][1], k[1][0])
-            filteredInfo.push(data[data.findIndex(s => s[k[1][1]] == k[1][0])])
+            filteredInfo.push(data.filter(s => s[k[1][1]] == k[1][0]))
         });
 
+        combined = filteredInfo[0].concat(...filteredInfo.slice(1, filteredInfo.length));
+
         // console.log(d3.event.target);
-        console.log(filteredInfo);
+        console.log(combined);
 
         var tbody = d3.select("tbody");
 
         d3.select("tbody").html("");
 
-        filteredInfo.forEach(function(filtered)
-            {
-                console.log(filtered);
-                var row = tbody.append("tr");
-                Object.entries(filtered).forEach(function([key, value])
-                    {
-                        console.log(key, value);
-                        var cell = row.append("td");
-                        cell.text(value);
-                    });
-            });
-        });
-    };
-
+        makeTable(combined)
+    });
+};
     
 button.on("click", PressButton)
+
